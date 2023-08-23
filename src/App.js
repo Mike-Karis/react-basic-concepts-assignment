524cf031a4574559a2dd7c6de1fa800ebfb6d501
Mike-Karis <mkaris1@my.wctc.edu>
2023-08-23 11:35:00 -0500

    Assignment2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
 import Country from './components/Country';
-import { render } from 'react-dom';
+
 
 class App extends Component{
+  state = {
+    countries: [
+      { id: 1, name: 'United States', gold: 2 },
+      { id: 2, name: 'China', gold: 3 },
+      { id: 3, name: 'Germany', gold: 0 },
+    ]
+  }
+  handleClick = (countriesId) => {
+    console.log(countriesId);
+    for (var i = 0, l = this.state.countries.length; i < l; i++) {
+      if(i+1 === countriesId){
+        console.log(this.state.countries[i]);
+        // eslint-disable-next-line
+        this.state.countries[i].gold = this.state.countries[i].gold+1;
+        console.log(this.state.countries[i]);
+      }
+  }
+    const countries = this.state.countries;
+    this.setState({ countries:countries });
+}
+loseGold = (countriesId) => {
+  console.log(countriesId);
+  for (var i = 0, l = this.state.countries.length; i < l; i++) {
+    if(i+1 === countriesId){
+      // eslint-disable-next-line
+      if(this.state.countries[i].gold != 0){
+      console.log(this.state.countries[i]);
+      // eslint-disable-next-line
+      this.state.countries[i].gold = this.state.countries[i].gold-1;
+      console.log(this.state.countries[i]);
+      }
+    }
+}
+  const countries = this.state.countries;
+  this.setState({ countries:countries });
+}
   render () {
   return (
     <div className="App">
       <header className="App-header">
-        <Country/>
+        { this.state.countries.map(country => 
+          <Country 
+            key={ country.id } 
+            id = { country.id }
+            name={ country.name }
+            gold={country.gold}
+            onIncrement={ this.handleClick } 
+            onDecrease={this.loseGold}
+            />
+        )}
       </header>
       
     </div>
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 class Country extends Component {
-    state = { 
-        name: 'United States',
-        goldmedals: 1,
-     } 
+    // state = { 
+    //     name: 'United States',
+    //     goldmedals: 1,
+    //  } 
+    // render() { 
+    //     return (<div><div><h1>{this.state.countries.name}</h1></div><div><h1>Gold Medals: {this.state.goldmedals} <button className='add'>+</button></h1></div></div>);
+    // }
     render() { 
-        return (<div><div><h1>{this.state.name}</h1></div><div><h1>Gold Medals: {this.state.goldmedals} <button className='add'>+</button></h1></div></div>);
-    }
-    handleClick = () => {
-        // console.log(this.state.goldmedals);
-        this.setState({ goldmedals: this.state.goldmedals + 1 })
-    }
-    render() { 
-        return (<div onClick={ this.handleClick } className='add'><div><div><h1>{this.state.name}</h1></div><div><h1>Gold Medals: {this.state.goldmedals} <button className='add'>+</button></h1></div></div></div>);
+        const { onIncrement, onDecrease, name, gold, id } = this.props;
+        return (<div className='Country'><div><h1>{name}</h1></div><div><h1>Gold Medals: {gold} <button onClick={ () => onIncrement(id) }>+</button><button onClick={ () => onDecrease(id) }>-</button></h1></div></div>);
     }
 }
  
